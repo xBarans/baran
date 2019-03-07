f1966923a5f0603024028600f6b0c39329d9812a
xBarans <48312562+xBarans@users.noreply.github.com>
2019-03-07 14:10:16 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bunu yapmaya yetkin yok!");
-  if(!args[0]) return message.channel.send("no");
+  if(!args[0]) return message.channel.send("hayır");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
